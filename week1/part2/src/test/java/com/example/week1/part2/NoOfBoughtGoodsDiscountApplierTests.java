477a252a86ca7bfa83146f520b1bed52ae3d39ee
rwalker <robpub@robonco.com>
2023-11-10 15:02:50 -0600

    Add tests for NoOfBoughtGoodsDiscountApplier

---
+++ week1/part2/src/test/java/com/example/week1/part2/NoOfBoughtGoodsDiscountApplierTests.java
@@ -0,0 +1,39 @@
+package com.example.week1.part2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class NoOfBoughtGoodsDiscountApplierTests {
+
+	private final int MIN_ITEM_COUNT_FOR_DISCOUNT = 5;
+	private final double NO_DISCOUNT = 0D;
+	private final double ITEM_COUNT_DISCOUNT = 5D;
+
+	private static NoOfBoughtGoodsDiscountApplier discountApplier;
+
+	@BeforeAll
+	static void initAll() {
+		discountApplier = new NoOfBoughtGoodsDiscountApplier();
+	}
+
+
+	@Test
+	void should_get_no_discount_for_too_few_items() {
+		Person tooFewItemsPerson = new Person("Homer",  MIN_ITEM_COUNT_FOR_DISCOUNT - 1, Occupation.EMPLOYED);
+
+		double discount = discountApplier.getDiscountRate(tooFewItemsPerson);
+
+		Assertions.assertEquals(NO_DISCOUNT, discount, 0.01D);
+	}
+
+	@Test
+	void should_get_discount_for_enough_items() {
+		Person enoughItemsPerson = new Person("Homer",  MIN_ITEM_COUNT_FOR_DISCOUNT, Occupation.EMPLOYED);
+
+		double discount = discountApplier.getDiscountRate(enoughItemsPerson);
+
+		Assertions.assertEquals(ITEM_COUNT_DISCOUNT, discount, 0.01D);
+	}
+
+}
