c22025f066c16d02ce43d4c4260594c48c66e67f
rwalker <robpub@robonco.com>
2023-11-19 09:40:00 -0600

    Implement AcceptanceTests

--- week2/part5/src/test/java/com/example/week2/part5/AcceptanceTests.java
+++ week2/part5/src/test/java/com/example/week2/part5/AcceptanceTests.java
@@ -5,6 +5,9 @@ import java.sql.SQLException;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class AcceptanceTests {
 
 	@Nested
@@ -12,6 +15,9 @@ class AcceptanceTests {
 		@Test
 		void should_throw_exception_when_not_enough_arguments_were_passed() {
 			// TODO: Check how <Week2Part5.main> works with not enough arguments
+			assertThatThrownBy(() -> Week2Part5.main(new String[] {"arg1", "arg2", "arg3", "arg4", "arg5"}))
+					.isInstanceOf(IllegalArgumentException.class)
+					.hasMessageContaining("Wrong number of arguments ");
 		}
 	}
 
@@ -20,12 +26,15 @@ class AcceptanceTests {
 
 		@Test
 		void should_calculate_maximum_discount() throws SQLException {
-			// TODO: Implement me
-			// given - a discount rate was inserted to the database
-			// when - calculate discount for a person
-			// then - max discount is 8 for name, 5 for goods, 10 for occupation - total 23
+			givenDiscountRateWasInDatabase("for employed", Occupation.EMPLOYED, 10D );
 
-			// hint: check the DbDiscountTesting class for helper methods
+			Discount discount = new Week2Part5(DB_USER, DB_PASSWORD, jdbcUrl()).calculateDiscount(
+					new Person("foo", 42, Occupation.EMPLOYED)
+			);
+
+			assertThat(discount.getRate())
+					.describedAs("max discount is 8 for name, 5 for goods, 10 for occupation - total 23")
+					.isEqualTo(23D);
 		}
 
 	}
